9d34a76a595a20c01e6ea92e99bd3b3cb29f97c1
Parker C <parkercode98@gmail.com>
2023-12-12 18:03:14 -0600

    Updated

---
+++ scripts/husky/pre-commit.js
@@ -0,0 +1,65 @@
+// -------------------------------------------------------------------------- //
+//-                           HUSKY PRE COMMIT HOOK                          -//
+// -------------------------------------------------------------------------- //
+// @ts-check
+import { confirm } from '@inquirer/prompts';
+import { $ } from 'execa';
+import figures from 'figures';
+import { createSpinner } from 'nanospinner';
+/* -------------------------------------------------------------------------- */
+// pnpm run awesome --update && git add .
+
+const confirmUpdate = await confirm({
+  message: 'Do you want to update the main README.md before commiting?',
+  default: true,
+});
+
+if (!confirmUpdate) process.exit(0);
+
+const spinner = createSpinner('Running pre-commit hook...').start();
+
+const updater = $`pnpm run awesome --update`;
+
+updater.stdout.on('data', (data) => spinner.update({ text: String(data) }));
+
+await new Promise((resolve, reject) => {
+  try {
+    updater.on('close', (code) => {
+      if (code === 0) {
+        resolve();
+      }
+      reject();
+    });
+
+    updater.on('error', (err) => {
+      reject(err);
+    });
+  } catch (error) {
+    reject(error);
+  }
+});
+
+spinner.update({ text: `${figures.tick} Main ReadMe Updated!` });
+
+const gitAddAll = $`git add .`;
+
+gitAddAll.stdout.on('data', (data) => spinner.update({ text: String(data) }));
+
+await new Promise((resolve, reject) => {
+  try {
+    gitAddAll.on('close', (code) => {
+      if (code === 0) {
+        resolve();
+      }
+      reject();
+    });
+
+    gitAddAll.on('error', (err) => {
+      reject(err);
+    });
+  } catch (error) {
+    reject(error);
+  }
+});
+
+spinner.update({ text: `${figures.tick} Done!` });
--- scripts/index.js
+++ scripts/index.js
@@ -28,37 +28,36 @@ const programOptions = program.opts();
 /* -------------------------------------------------------------------------- */
 let answer;
 if (!!programOptions?.create) {
-  answer = './make-new-list.js';
+  answer = `node ${path.join(__dirname, 'make-new-list.js')}`;
 } else if (!!programOptions?.update) {
-  answer = './update-main.js';
+  answer = `node ${path.join(__dirname, 'update-main.js')}`;
 } else {
   answer = await select({
     message: 'What would you like to do?',
     choices: [
-      { value: './make-new-list.js', name: 'Create a new list', description: 'Create a new awesome list' },
       {
-        value: './update-main.js',
+        value: `node ${path.join(__dirname, 'make-new-list.js')}`,
+        name: 'Create a new list',
+        description: 'Create a new awesome list',
+      },
+      {
+        value: `node ${path.join(__dirname, 'update-main.js')}`,
         name: 'Update Main README.md',
         description: 'Update the main README.md file with the latest lists',
       },
+      { value: `pnpm run dev`, name: 'Start Development Server', description: 'Start Development Server' },
       { name: '🍿', value: 'exit', description: 'Just looking around...(exit)' },
     ],
   });
 }
-
+// Start Development Server
 if (answer === 'exit') {
   console.clear();
   process.exit(0);
 }
 
 try {
-  const filePath = path.join(__dirname, answer);
-
-  const fileExists = await fse.exists(filePath);
-
-  if (!fileExists) throw new Error('File does not exist');
-
-  await $$`node ${filePath}`;
+  await $$`${answer}`;
 } catch (err) {
   console.log(`\n${c.red`Oops!`} ${c.yellow`(⚆_⚆)`}\n`.concat(c.red(`Something went wrong.\n${err?.message ?? ''}`)));
   process.exit(0);
